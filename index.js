e67e22a8d9a806a4c2a705a50f71642fda7a5682
Marga <margalppalacios@gmail.com>
2022-06-07 20:20:19 +0200

    evento de eliminar video

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const express = require('express');
+const path = require('path');
 
 
 const app = express()
@@ -9,14 +10,29 @@ const port = 3000
 
 app.get('/video/:parametro', (req, res) =>
 {
-    let parametro = req.params.parametro
+    let parametro = req.params.parametro;
+    console.log(parametro);
+    var linkName = 'video_' + parametro +'.mp4';
+    console.log(linkName);
     ytdl('http://www.youtube.com/watch?v='+ parametro)
-        .on('finish', function(){ res.download(__dirname+'\\video.mp4')})
-        .pipe(fs.createWriteStream(parametro +'.mp4')); 
-
+        .on('finish', function()
+        {
+          res.download(__dirname+path.sep +linkName)
+            res.on('finish', function()
+            {
+               //eliminar videos de la carpeta
+              try {
+                fs.unlinkSync(__dirname+path.sep +linkName);
+              } catch(err) {
+                console.error(err)
+              }
+            })
+        })
+        .pipe(fs.createWriteStream(linkName)); 
+    
 });
 
 app.listen(port, () =>
 {
   console.log(`Example app listening on port ${port}`)
-})
+});
