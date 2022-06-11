5d07e2c1f11e074d590c616838d89beb84ac475b
Marga <margalppalacios@gmail.com>
2022-06-11 11:19:09 +0200

    registro de usuarios

---
+++ db/iniciar_sesion.js
@@ -0,0 +1,33 @@
+var mysql = require('mysql');
+const envData= require('../.env.json'); 
+
+var con = mysql.createConnection({
+  host: envData.host,
+  user: envData.user,
+  password: envData.password,
+  database: "ytdownload"
+});
+
+
+con.connect(function(err) 
+{
+  if (err) throw err;
+
+  var User = {
+    user: 'ramoncite',
+    hash: 'bed6424efd434a84cd5e96172835ffe8cb72632b2e1b'
+  }
+
+  var queryIns = `SELECT * FROM ytdownload.users
+  WHERE 
+  user =   '${User.user}' and 
+  hash =   '${User.hash}' `;
+
+
+
+  con.query(queryIns, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+
+});
